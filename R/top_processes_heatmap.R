785ef804cd00ee0ae5a0a7d6558cb8796bff9d8b
Dien Nguyen <dien.nguyen@mail.utoronto.ca>
2023-11-14 12:18:38 -0500

    add new arguments and edit documentation

--- R/top_processes_heatmap.R
+++ R/top_processes_heatmap.R
@@ -1,17 +1,38 @@
 #' Produce a heatmap of the top processes
 #'
 #' This function creates and returns a heatmap showing the top processes in each
-#' cluster.
+#' cluster. It automatically saves the plot as a .png file in the current
+#' working directory.
 #'
 #' @param top_processes_df A data frame containing the top processes of each
 #' cluster. The rownames should be the processes and the column names should
 #' be the cluster. It can be produced using the get_all_top_processes function
 #' in the package.
+#' @param width The width of the output file
+#' @param height The height of the output file
 #' @param cellwidth The cell width for each cell in the heatmap.
 #' @return A heatmap that represents the top processes in each cluster.
 #'
 #' @export
-top_processes_heatmap <- function(top_processes_df, cellwidth = 30) {
+#' @import pheatmap
+
+top_processes_heatmap <- function(top_processes_df, width = 12, height = 6,
+                                  cellwidth = 30) {
+  # check that cellwidth is > 0
+  if (cellwidth <= 0) {
+    stop("Cellwidth provided must be greater than 0.")
+  }
+
+  # check that width and height are both > 0
+  if (width < 0 || height < 0) {
+    stop("Both width and height must be greater than 0.")
+  }
+
+  # check that row names are GO processes
+  if (!all(grepl("^GO:", rownames(top_processes_df)))) {
+    stop("Each rowname must be a GO process, for example, GO:0005829~cytosol.")
+  }
+
   # pheatmap function doesn't take NA values, so convert all NAs to 0
   top_processes_df[is.na(top_processes_df)] <- 0
   top_processes_df[] <- lapply(top_processes_df, as.numeric)
@@ -26,7 +47,11 @@ top_processes_heatmap <- function(top_processes_df, cellwidth = 30) {
                            cluster_rows = F,
                            cluster_cols = F,
                            border_color = "black",
-                           cellwidth = cellwidth)
+                           cellwidth = cellwidth,
+                           main = "Heatmap of top processes of each cluster",
+                           filename = "./top_process_heatmap.png",
+                           width = width,
+                           height = height)
   return(heatmap_plot)
 }
 # [END]
