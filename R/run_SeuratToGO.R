4316707b337d3fba49422bd7a901a39d8e4db537
Dien Nguyen <dien.nguyen@mail.utoronto.ca>
2023-12-11 01:11:08 -0500

    move app.R file to shiny folder

--- R/run_SeuratToGO.R
+++ R/run_SeuratToGO.R
@@ -1,11 +1,13 @@
 #' Run the package's Shiny App
 #'
 #' This function runs the package's Shiny App
+#' @importFrom shiny shinyAppDir
+#' @importFrom shiny runApp
 #'
 #' @export
 
 run_SeuratToGO <- function() {
-  appDir <- system.file("R",
+  appDir <- system.file("shiny-scripts",
                         package = "SeuratToGO")
   shinyObj <- shiny::shinyAppDir(appDir)
   shiny::runApp(shinyObj, display.mode = "normal")
